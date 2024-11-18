3c88c91e2cfd4dd32314feb0e64fbff872a4b044
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2024-11-18 20:24:18 +0530

    multer - uploading single and multiple file

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -3,31 +3,40 @@ const app = express();
 
 const path = require("path");
 
-const multer = require('multer');
+const multer = require("multer");
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 const upload = multer({ storage: storage });
-
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
 
-
 app.get("/", (req, res) => {
   return res.render("home");
 });
 
-app.post("/upload", upload.single('image'), (req, res) => {
-  return res.redirect("/");
-});
+app.post(
+  "/upload",
+  upload.fields([{ name: "image", maxCount: 1 }, { name: "image2" }]),
+  (req, res) => {
+    return res.redirect("/");
+  }
+);
 
+// app.post(
+//   "/upload",
+//   upload.single("image"),
+//   (req, res) => {
+//     return res.redirect("/");
+//   }
+// );
 
 app.listen(8000, () => console.log("Server connected"));
